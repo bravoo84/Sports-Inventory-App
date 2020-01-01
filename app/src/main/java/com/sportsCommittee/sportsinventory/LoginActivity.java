5ce4293df90cf120412f9b7e794ba469da2ca748
bhavik <bhavikmehta98@gmail.com>
2020-01-01 17:02:18 +0530

    login-page backend completed!

--- app/src/main/java/com/sportsCommittee/sportsinventory/LoginActivity.java
+++ app/src/main/java/com/sportsCommittee/sportsinventory/LoginActivity.java
@@ -1,18 +1,46 @@
 package com.sportsCommittee.sportsinventory;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
 
 
-
     Toolbar toolbar;
+    EditText et_username, et_password;
+    String username, password;
+    Button login;
+    ProgressBar progressBar;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+
+    ConnectivityCheck connectivityCheck;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!isInternetConnected()) return;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +48,97 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
+        init();
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-        toolbar.setTitle("Login");
-        toolbar.setTitleTextColor(getResources().getColor(R.color.textColorLight));
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!isInternetConnected()) return;
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                username = et_username.getText().toString();
+                password = et_password.getText().toString();
+
+                firebaseAuth.signInWithEmailAndPassword(username, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+
+                                if (task.isSuccessful()) {
+
+                                    user = firebaseAuth.getCurrentUser();
+
+                                    Toast.makeText(LoginActivity.this, "Logged in!", Toast.LENGTH_LONG).show();
+
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(intent);
+
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Incorrect User Id/Password!", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+        });
 
 
     }
 
+    public void init() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        et_username = (EditText) findViewById(R.id.username);
+        et_password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+        toolbar.setTitle("Login");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.textColorLight));
+        setSupportActionBar(toolbar);
+
+    }
+
+
+    public boolean isInternetConnected() {
+
+        if (!ConnectivityCheck.isNetworkConnected(getApplicationContext())) {
+            startActivity(new Intent(LoginActivity.this, NoConnection.class));
+            return false;
+        }
+        return true;
+    }
+
 
 }
+
+
+
+
+
+
+
+
+
+
+//          login.setOnClickListener(new View.OnClickListener() {
+//              @Override
+//              public void onClick(View v) {
+//                  firebaseAuth.sendPasswordResetEmail(et_username.getText().toString())
+//                          .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                              @Override
+//                              public void onComplete(@NonNull Task<Void> task) {
+//                                  if(task.isSuccessful()){
+//                                      Toast.makeText(LoginActivity.this,"Link sent!",Toast.LENGTH_LONG).show();
+//                                  }else{
+//                                      Toast.makeText(LoginActivity.this,"Failed !",Toast.LENGTH_LONG).show();
+//                                  }
+//                              }
+//                          });
+//              }
+//          });
+
--- app/src/main/java/com/sportsCommittee/sportsinventory/MainActivity.java
+++ app/src/main/java/com/sportsCommittee/sportsinventory/MainActivity.java
@@ -2,13 +2,54 @@ package com.sportsCommittee.sportsinventory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(!validation())return;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public boolean validation(){
+        if(!isInternetConnected()|| !isLoggedIn()) return true;
+        return false;
+    }
+
+
+    public boolean isInternetConnected(){
+
+        if(!ConnectivityCheck.isNetworkConnected(getApplicationContext())){
+            startActivity(new Intent(MainActivity.this,NoConnection.class));
+            return false;
+        }
+        return true;
+    }
+
+
+    public boolean isLoggedIn(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(user==null){
+
+            Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            return false;
+        }
+
+        return true;
+    }
+
 }
